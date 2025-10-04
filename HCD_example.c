6b58d1a98ba0cdc410a264c69415e6a1a3231c64
ProgrammingLouis <louis.programer@gmail.com>
2025-10-04 20:29:58 +0200

    Fixed file read & Added filter to example
    Note: Removed write_pgm_file_16bits because format is not well read by
    some app

--- HCD_example.c
+++ HCD_example.c
@@ -4,10 +4,66 @@
 
 #include "file_helper.h"
 
+int apply_kernel_single_block(int input[3][3]) {
+    // Somme des valeurs dans la fen�tre 3x3, chaque pixel est pond�r� � 1
+    int sum = 0;
+    sum += input[0][0];
+    sum += input[0][1];
+    sum += input[0][2];
+    sum += input[1][0];
+    sum += input[1][1];
+    sum += input[1][2];
+    sum += input[2][0];
+    sum += input[2][1];
+    sum += input[2][2];
+
+    // Retourne la somme des pixels voisins
+    return sum/9;
+}
+
+void HCD_filter(uint dimy, uint dimx, int16_t I_x[dimy][dimx], int16_t I_y[dimy][dimx], uint8_t output_img[dimy][dimx]) {
+    int S_x2[dimy][dimx];
+    int S_y2[dimy][dimx];
+    int S_xy[dimy][dimx];
+
+    // Calcul pour chaque pixel de l'image enti�re
+    for (int y = 1; y < dimy - 1; y++) {
+    	for (int x = 1; x < dimx - 1; x++) {
+            // Calcul des produits Ix^2, Iy^2, Ix*Iy pour les fen�tres de lissage
+            int Ix2_window[3][3];
+            int Iy2_window[3][3];
+            int Ixy_window[3][3];
+
+            // Remplir les fen�tres de produits pour le lissage
+            for (int ky = -1; ky <= 1; ky++) {
+                for (int kx = -1; kx <= 1; kx++) {
+                    int yy = y + ky;
+                    int xx = x + kx;
+                    Ix2_window[ky + 1][kx + 1] = (int)(I_x[yy][xx] * I_x[yy][xx]);
+                    Iy2_window[ky + 1][kx + 1] = (int)(I_y[yy][xx] * I_y[yy][xx]);
+                    Ixy_window[ky + 1][kx + 1] = (int)(I_x[yy][xx] * I_y[yy][xx]);
+                }
+            }
+
+            // Appliquer le noyau de lissage sur les produits calcul�s
+            S_x2[y][x] = apply_kernel_single_block(Ix2_window);
+            S_y2[y][x] = apply_kernel_single_block(Iy2_window);
+            S_xy[y][x] = apply_kernel_single_block(Ixy_window);
+
+            // Calcul de la r�ponse Harris
+            double det_M = (double)(S_x2[y][x] * S_y2[y][x]) - (double)(S_xy[y][x] * S_xy[y][x]);
+            double trace_M = (double)(S_x2[y][x] + S_y2[y][x]);
+            double R = det_M - 0.05 * (trace_M * trace_M);
+
+            output_img[y][x] = R/2 > 255.0 ? (uint8_t)(255) : R/2 < 0.0 ? 0 : (uint8_t)(R/2);
+        }
+    }
+}
+
 int main(void)
 {
   // Read colored input image
-  ppm_file_t input_ppm_file = read_ppm_file("bclc.ppm");
+  ppm_file_t input_ppm_file = read_ppm_file("Text_company_logo.ppm");
   if (input_ppm_file.dimy == 0 || input_ppm_file.dimx == 0 || !input_ppm_file.pixels)
     return EXIT_FAILURE;
   uint dimy = input_ppm_file.dimy;
@@ -22,7 +78,7 @@ int main(void)
     for (uint x = 0; x < dimx; ++x)
     {
       uint8_t* colors = pixels[y][x];
-      uint8_t gray_scale_val = (uint8_t)(0.299*(double)colors[0] + 0.587*(double)colors[1] + 0.114*(double)colors[2]);
+      uint8_t gray_scale_val = (uint8_t)(0.299f*(float)colors[0] + 0.587f*(float)colors[1] + 0.114f*(float)colors[2]);
       grayscale_pixels[y][x] = gray_scale_val;
     }
   }
@@ -35,39 +91,51 @@ int main(void)
     return EXIT_FAILURE;
   
   // Get Iy and Ix
-  uint8_t (*Iy_pixels)[input_ppm_file.dimx] = malloc(dimy*dimx * sizeof(uint8_t));
-  uint8_t (*Ix_pixels)[input_ppm_file.dimx] = malloc(dimy*dimx * sizeof(uint8_t));
+  int16_t (*Iy_pixels)[input_ppm_file.dimx] = malloc(dimy*dimx * sizeof(int16_t));
+  int16_t (*Ix_pixels)[input_ppm_file.dimx] = malloc(dimy*dimx * sizeof(int16_t));
+  uint8_t (*abs_Iy_pixels)[input_ppm_file.dimx] = malloc(dimy*dimx * sizeof(uint8_t));
+  uint8_t (*abs_Ix_pixels)[input_ppm_file.dimx] = malloc(dimy*dimx * sizeof(uint8_t));
 
-  for (uint y = 1; y < dimy-1; ++y)
+  for (uint y = 0; y < dimy; ++y)
   {
-    for (uint x = 1; x < dimx-1; ++x)
+    for (uint x = 0; x < dimx; ++x)
     {
-      Iy_pixels[y][x] = grayscale_pixels[y+1][x] - grayscale_pixels[y-1][x];
-      Ix_pixels[y][x] = grayscale_pixels[y][x+1] - grayscale_pixels[y][x-1];
+      // TODO: Is this the most optimized ?
+      if (y == 0 || y == dimy-1)
+        Iy_pixels[y][x] = 0;
+      else
+        Iy_pixels[y][x] = grayscale_pixels[y+1][x] - grayscale_pixels[y-1][x];
+      
+      if (x == 0 || x == dimx-1)
+        Ix_pixels[y][x] = 0;
+      else
+        Ix_pixels[y][x] = grayscale_pixels[y][x+1] - grayscale_pixels[y][x-1];
+      abs_Iy_pixels[y][x] = (uint8_t)((Iy_pixels[y][x] + 255)/2);
+      abs_Ix_pixels[y][x] = (uint8_t)((Ix_pixels[y][x] + 255)/2);
     }
   }
   free(grayscale_pixels);
 
-  for (uint x = 0; x < dimx; ++x)
-  {
-    Iy_pixels[0][x] = 0;
-    Ix_pixels[dimy-1][x] = 0;
-  }
-  for (uint y = 0; y < dimy; ++y)
-  {
-    Iy_pixels[y][0] = 0;
-    Ix_pixels[y][dimx-1] = 0;
-  }
+  write_error = write_pgm_file("Iy.pgm", dimy, dimx, (uint8_t*)abs_Iy_pixels);
+  if (write_error)
+    return EXIT_FAILURE;
+  write_error = write_pgm_file("Ix.pgm", dimy, dimx, (uint8_t*)abs_Ix_pixels);
+  if (write_error)
+    return EXIT_FAILURE;
 
-  write_error = write_pgm_file("Iy.pgm", dimy, dimx, (uint8_t*)Iy_pixels);
+  uint8_t (*output_img)[dimx] = malloc(dimy*dimx * sizeof(uint8_t));
+  HCD_filter(dimy, dimx, Ix_pixels, Iy_pixels, output_img);
+
+  write_error = write_pgm_file("out.pgm", dimy, dimx, (uint8_t*)output_img);
   if (write_error)
     return EXIT_FAILURE;
-  write_error = write_pgm_file("Ix.pgm", dimy, dimx, (uint8_t*)Ix_pixels);
-  if (write_error)
-    return EXIT_FAILURE;
+  
+  free(output_img);
 
   free(Iy_pixels);
   free(Ix_pixels);
+  free(abs_Iy_pixels);
+  free(abs_Ix_pixels);
   
   return EXIT_SUCCESS;
 }
--- file_helper.c
+++ file_helper.c
@@ -1,4 +1,24 @@
+#include <string.h>
+#include <ctype.h>
+
 #include "file_helper.h"
+
+int read_token(FILE *fp, char *buf, size_t size) {
+    int c;
+
+    // Skip whitespace and comments
+    do {
+        c = fgetc(fp);
+        if (c == '#') {  // skip comment line
+            while (c != '\n' && c != EOF) c = fgetc(fp);
+        }
+    } while (isspace(c));
+
+    if (c == EOF) return 0;
+    ungetc(c, fp);
+
+    return fscanf(fp, "%99s", buf);  // read token
+}
 
 ppm_file_t read_ppm_file(char *filename) {
   printf("Openning file %s ...\n", filename); 
@@ -11,10 +31,43 @@ ppm_file_t read_ppm_file(char *filename) {
     printf("Failed to open file !\n"); 
     return error_struct;
   }
-  int param_read = fscanf(input_file, "P6\n%u %u\n255\n", &ppm_file.dimx, &ppm_file.dimy);
-  if (param_read != 2) {
-    printf("Unsupported file format !\n");
-    return error_struct;
+
+    char buf[100];
+
+    // Magic number
+    if (!read_token(input_file, buf, sizeof(buf))) return error_struct;
+    if (strcmp(buf, "P6") != 0) {
+      printf("Invalid file format : Expected \"P6\" !\n");
+      return error_struct;
+    }
+
+    // Width
+    if (!read_token(input_file, buf, sizeof(buf))) return error_struct;
+    ppm_file.dimx = atoi(buf);
+    if (ppm_file.dimx == 0) {
+      printf("Invalid file format : Expected integer for dimx !\n");
+      return error_struct;
+    }
+
+    // Height
+    if (!read_token(input_file, buf, sizeof(buf))) return error_struct;
+    ppm_file.dimy = atoi(buf);
+    if (ppm_file.dimy == 0) {
+      printf("Invalid file format : Expected integer for dimy !\n");
+      return error_struct;
+    }
+
+    // Maxval
+    if (!read_token(input_file, buf, sizeof(buf))) return error_struct;
+    if (atoi(buf) != 255) {
+      printf("Invalid file format : Expected maxval == 255 !\n");
+      return error_struct;  // only 8-bit
+    }
+  
+  int c = fgetc(input_file);
+  if (c != '\n') {
+    printf("Invalid file format : Expected new line after maxval !\n");
+    return error_struct;  // only 8-bit
   }
 
   printf("Image dimensions : %u x %u\n", ppm_file.dimx, ppm_file.dimy);
@@ -31,7 +84,7 @@ ppm_file_t read_ppm_file(char *filename) {
   return ppm_file;
 }
 
-uint write_pgm_file(char *filename, uint dimy, uint dimx, uint8_t*pixels) {
+uint write_pgm_file(char *filename, uint dimy, uint dimx, uint8_t *pixels) {
   printf("Writing file %s ...\n", filename); 
 
   FILE *output_file = fopen(filename, "wb");
@@ -47,26 +100,6 @@ uint write_pgm_file(char *filename, uint dimy, uint dimx, uint8_t*pixels) {
     return 1;
   }
 
-  fclose(output_file);
-  return 0;
-}
-
-uint write_pgm_file_16bits(char *filename, uint dimy, uint dimx, uint16_t*pixels) {
-  printf("Writing file %s ...\n", filename); 
-
-  FILE *output_file = fopen(filename, "wb");
-  if (!output_file) {
-    printf("Failed to open file !\n"); 
-    return 1;
-  }
-
-  fprintf(output_file, "P5\n%u %u\n65535\n", dimx, dimy);
-  size_t blocks_written = fwrite(pixels, 2*dimy*dimx, 1, output_file);
-  if (blocks_written != 1) {
-    printf("Failed to write pixel data !\n");
-    return 1;
-  }
-  
   fclose(output_file);
   return 0;
 }
